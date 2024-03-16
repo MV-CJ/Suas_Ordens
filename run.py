872861f3f8d83cb901c506f740ccb05b32699e74
Marcos Jesus <marcos.jesus@weduu.com>
2024-03-16 17:30:09 -0300

    Finalmente fix para salvar no postgres

--- run.py
+++ run.py
@@ -3,8 +3,6 @@ import os
 
 if __name__ == "__main__":
     # Define a porta na qual o Flask irá escutar
-    port = int(os.environ.get('PORT', 5000))
-    # Cria o aplicativo Flask
+    port = int(os.environ.get('PORT', 5432))
     app = create_app()
-    # Inicia o aplicativo Flask
     app.run(host='0.0.0.0', port=port)
